058900df3d8ae90c43cd755dc70cc59c7d96670d
db556b <102768499+db556b@users.noreply.github.com>
2022-01-13 14:53:57 -0500

    daily CW
    added Kata:
    Squash the bugs

---
+++ 011322/main.js
@@ -0,0 +1,26 @@
+// Squash the bugs
+
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+// There will only be one 'longest' word.
+
+
+function findLongest(str) {
+  
+    var spl = str.split(" ");
+    var longest = 0
+    
+    for (var i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length
+      }
+      }
+      return longest
+  }
+
+
+  //refactored using pertinent methods
+  
+  function findLongest2(str) {
+    return Math.max(...str.split(' ').map(i => i.length))
+  }
