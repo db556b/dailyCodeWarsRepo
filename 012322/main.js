9fa49ef559dc44672549c2f657002985c774b821
db556b <102768499+db556b@users.noreply.github.com>
2022-01-23 09:24:24 -0500

    daily CW
    added kata:
    Ensure question

---
+++ 012322/main.js
@@ -0,0 +1,9 @@
+
+
+// Ensure question
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+
+function ensureQuestion(s) {
+    return s[s.length - 1] === '?' ? s : s + '?'
+  }
