555c16f20fe82f9c51404d9055560fe96cd56e19
db556b <102768499+db556b@users.noreply.github.com>
2022-01-26 10:00:12 -0500

    Update main.js

--- 012622/main.js
+++ 012622/main.js
@@ -1,4 +1,7 @@
-// parameters
-// return
-// examples
-// PseudoCode
+
+// Price of Mangoes
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+function mango(q, p){
+    return (Math.floor(q / 3) * 2 * p) + (q % 3 * p);
+  }
