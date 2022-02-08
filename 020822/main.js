5d187f132b06097d01b85eb2390d7214b3fe452b
db556b <102768499+db556b@users.noreply.github.com>
2022-02-08 10:23:43 -0500

    daily Cw update
    added kata:
    The falling speed of petals

---
+++ 020822/main.js
@@ -0,0 +1,11 @@
+
+The falling speed of petals
+When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April.
+
+Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch.
+
+Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+
+function sakuraFall(v) {
+    return v > 0 ? 400 / v : 0
+   }
