66bac1a026d1aae62f69aa828b24cb349474f5c5
db556b <102768499+db556b@users.noreply.github.com>
2022-02-09 10:31:28 -0500

    daily CW update
    added kata:
    Parse float

---
+++ 020922/main.js
@@ -0,0 +1,8 @@
+
+
+// Parse float
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+function parseF(s) {
+    return isNaN(parseFloat(s)) ? null : parseFloat(s);
+  }
