ce688f75afdae251dcca4c3f7a7e29cde28a5339
db556b <davidbuchan900@gmail.com>
2022-03-23 12:51:59 -0400

    daily CW update
    added kata:
    Is integer safe to use?

---
+++ 032322/main.js
@@ -0,0 +1,8 @@
+
+
+
+// Is integer safe to use?
+// Not all integers can be represented by JavaScript/TypeScript. It has space to to represent 53bit signed integers. In this Kata, we've to determine if it is safe to use the integer or not. Make use of the latest ES6 features to find this.
+
+
+SafeInteger = n => n <= 9007199254740991
