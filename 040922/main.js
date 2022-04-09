240fd1be4a274d282febfa7923b85d421ef73268
db556b <davidbuchan900@gmail.com>
2022-04-09 09:10:08 -0400

    daily CCW update
    added kata: Formatting decimal places #0

---
+++ 040922/main.js
@@ -0,0 +1,8 @@
+
+// Formatting decimal places #0
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+
+function twoDecimalPlaces(n) {
+    return Math.round(n*100)/100
+  }
