8e4544315725f48ebc98a74698448421f8b23d7f
db556b <102768499+db556b@users.noreply.github.com>
2022-04-19 14:11:53 -0400

    Update main.js

---
+++ 041922/js/main.js
@@ -0,0 +1,63 @@
+// Create a function that will take any amount of money and break it down to the smallest number of bills as possible. Only integer amounts will be input, NO floats. This function should output a sequence, where each element in the array represents the amount of a certain bill type. The array will be set up in this manner:
+
+// array[0] ---> represents $1 bills
+
+// array[1] ---> represents $5 bills
+
+// array[2] ---> represents $10 bills
+
+// array[3] ---> represents $20 bills
+
+// array[4] ---> represents $50 bills
+
+// array[5] ---> represents $100 bills
+
+// In the case below, we broke up $365 into 1 $5 bill, 1 $10 bill, 1 $50 bill, and 3 $100 bills.
+
+// giveChange(365) // =>  [0,1,1,0,1,3]
+
+// In this next case, we broke $217 into 2 $1 bills, 1 $5 bill, 1 $10 bill, and 2 $100 bills.
+
+// giveChange(217) // => [2,1,1,0,0,2]
+
+
+function giveChange(amount) {
+    let arr = []
+    for (let i = 5; i>=0; i--){
+      if (i === 5){
+        arr[i] = Math.floor(amount/100)
+        amount = amount%100
+      }
+      if (i === 4){
+        arr[i] = Math.floor(amount/50)
+        amount = amount%50
+      }
+       if (i === 3){
+        arr[i] = Math.floor(amount/20)
+        amount = amount%20
+      }
+       if (i === 2){
+        arr[i] = Math.floor(amount/10)
+        amount = amount%10
+      }
+       if (i === 1){
+        arr[i] = Math.floor(amount/5)
+        amount = amount%5
+      }
+       if (i === 0){
+        arr[i] = Math.floor(amount/1)
+      }
+    }
+    return arr
+  }
+
+  function giveChange(amount) {
+    let arr = []
+    let cash = [1,5,10,20,50,100]
+    for (let i = 5; i>=0 ; i--){
+      let math = Math.floor(amount/cash[i])
+      arr [i] = math > 0 ? math : 0
+      amount %= cash[i]
+    }
+    return arr
+  }
