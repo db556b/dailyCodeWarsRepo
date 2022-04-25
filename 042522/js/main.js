b52b109d34b409504cedabb6c68c6d0db9936f29
db556b <102768499+db556b@users.noreply.github.com>
2022-04-25 12:16:20 -0400

    Added Kata: circle area inside square

--- 042522/js/main.js
+++ 042522/js/main.js
@@ -19,4 +19,19 @@ function makePassword(phrase) {
            .replace(/s/ig,'5')
   }
 
+//    Circle Area inside square
+//   Turn an area of a square in to an area of a circle that fits perfectly inside the square.
+
+//   inscribed circle
   
+//   You get the blue+red area and need to calculate the red area.
+  
+//   Your function gets a number which represents the area of the square and should return the area of the circle. The tests are rounded by 8 decimals to make sure multiple types of solutions work.
+  
+//   You don't have to worry about error handling or negative number input: area >= 0.
+  
+//   This kata might be simpler than you expect, but good luck!
+
+  function squareAreaToCircle(size){
+    return Math.PI * Math.pow(Math.sqrt(size)/2,2)
+  }
