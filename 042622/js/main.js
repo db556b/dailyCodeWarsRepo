7de79b64e7174e3a03e77e053df29853d69205e2
db556b <102768499+db556b@users.noreply.github.com>
2022-04-26 10:23:48 -0400

    CW update
    added Kata:
    Alphabet Symmetry

--- 042622/js/main.js
+++ 042622/js/main.js
@@ -16,3 +16,32 @@ function capitalize(s){
   };
 
   
+  //ALPHABET SYMMETRY
+  
+//   Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+//   Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+  
+//   solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+  
+//   See test cases for more examples.
+  
+//   Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+  
+//   Good luck!
+
+  function solve(arr){  
+    const str = `abcdefghijklmnopqrstuvwxyz`
+    let arr2= []
+    for (let i = 0; i < arr.length; i++ ){
+      arr2[i] = 0
+    }
+    for (let i = 0; i < arr.length ; i++){
+      for (let j = 0; j<arr[i].length ; j++){
+        if ([...arr[i]][j].toLowerCase() === [...str][j]){
+          arr2[i] += 1
+        }
+      }
+    }
+      return arr2
+    };
