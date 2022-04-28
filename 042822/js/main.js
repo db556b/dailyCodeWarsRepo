e86656aec3eca42d8e0cbd8778644ffeb1369f31
db556b <102768499+db556b@users.noreply.github.com>
2022-04-28 19:05:33 -0400

    Cw update
    Added title of kata in JS file

--- 042822/js/main.js
+++ 042822/js/main.js
@@ -1,3 +1,4 @@
+//Working with arrays I (and why your code fails in some katas)
 // In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
 
 // Good luck!
