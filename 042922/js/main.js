3995b1fec7e583be99c93df6b7e843e796c784e0
db556b <102768499+db556b@users.noreply.github.com>
2022-04-29 13:32:06 -0400

    CW daily
    added kata:
    Reversed Sequence

--- 042922/js/main.js
+++ 042922/js/main.js
@@ -97,3 +97,15 @@ var humanYearsCatYearsDogYears = function(humanYears) {
   function betterThanAverage(classPoints, yourPoints) {
     return classPoints.reduce((a,b) => a + b , yourPoints)/classPoints.length <= yourPoints
   }
+//  Reversed Sequence
+//   Build a function that returns an array of integers from n to 1 where n>0.
+
+//   Example : n=5 --> [5,4,3,2,1]
+  
+
+
+  const reverseSeq = n => {
+    let arr = []
+    for (let i = 1; i <= n; i++) arr.push(i)
+    return arr.reverse()
+  };
