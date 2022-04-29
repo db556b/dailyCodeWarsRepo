75d7849c61273398a92bdab9ba0758e4074f4252
db556b <102768499+db556b@users.noreply.github.com>
2022-04-29 13:21:23 -0400

    CW Update
    Added kata:
    Help the Elite Squad of Brazilian forces BOPE

--- 042922/js/main.js
+++ 042922/js/main.js
@@ -49,3 +49,36 @@ var humanYearsCatYearsDogYears = function(humanYears) {
      years(humanYears)
      return [humanYears,catYears,dogYears]
    }
+//      Help the Elite Squad of Brazilian forces BOPE
+//    The BOPE is the squad of special forces of police that usually handles the operations in the Favelas in Rio de Janeiro.
+
+//    In this Kata you have to write a function that determine the number of magazines that every soldier has to have in his bag.
+   
+//    You will receive the weapon and the number of streets that they have to cross. Considering that every street the officer shoots 3 times. Bellow there is the relation of weapons:
+   
+//    PT92 - 17 bullets
+//    M4A1 - 30 bullets
+//    M16A2 - 30 bullets
+//    PSG1 - 5 bullets
+   
+//    Example:
+   
+//    ["PT92", 6] => 2 (6 streets 3 bullets each)
+//    ["M4A1", 6] => 1
+   
+//    The return Will always be an integer so as the params.
+   
+
+   function magNumber(info){
+    const guns = {
+      PT92 : 17,
+      M4A1 : 30,
+      M16A2 : 30,
+      PSG1 : 5,
+      }
+    function numberOfClips(gun,streets){
+      let noOfBullets = streets * 3
+      return Math.ceil(noOfBullets/gun)
+    }
+    return numberOfClips(guns[info[0]],info[1])
+  }
