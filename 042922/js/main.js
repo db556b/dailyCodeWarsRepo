b276b5968977078086d11f6971bc5a6877d66bb7
db556b <102768499+db556b@users.noreply.github.com>
2022-04-29 12:48:38 -0400

    CW Daily
    Added Katas:
    Bin to Decimal
    CatYears, dogYears

---
+++ 042922/js/main.js
@@ -0,0 +1,4 @@
+//Bin to Decimal
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+const binToDec=(bin)=> parseInt(bin,2)
+console.log(binToDec(`1001000010101010101000111100011010101`))
