01a6416b09ebb7a9dda39d37fe1de8d35069d7be
db556b <102768499+db556b@users.noreply.github.com>
2022-05-02 12:41:40 -0400

    CW daily update
    added Kata:
    Credit Card Mask

--- 050222/js/main.js
+++ 050222/js/main.js
@@ -92,5 +92,21 @@ function eachCons(array, n) {
 
 
 function finalGrade (exam, projects) {
-    return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >=2 ? 75 : 0
+    return exam > 90 || projects > 10 ? 100 : 
+    exam > 75 && projects >= 5 ? 90 : 
+    exam > 50 && projects >=2 ? 75 
+    : 0
+  }
+
+//   Credit Card Mask
+
+//   Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+//   Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+  function maskify(cc) {
+    return cc
+      .split("")
+      .map((e, i) => (i > cc.length - 5 ? e : "#"))
+      .join("");
   }
