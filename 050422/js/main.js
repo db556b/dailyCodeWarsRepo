72f9e6d568531822fab1f11329d19d1d62f214cb
db556b <102768499+db556b@users.noreply.github.com>
2022-05-04 12:09:13 -0400

    CW daily update
    Added Kata:
    Sort Santa's Reindeer

--- 050422/js/main.js
+++ 050422/js/main.js
@@ -53,4 +53,23 @@ const xMarksTheSpot = (input) => {
             .join(" ")
       }
 
-      //test
+
+    //   Sort Santa's Reindeer
+    //   Happy Holidays fellow Code Warriors!
+
+    //   Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+    //   Sort Santa's Reindeer
+      
+    //   Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+    //   Notes:
+      
+    //       It's guaranteed that each string is composed of two words
+    //       In case of two identical last names, keep the original order
+      
+
+      function sortReindeer(reindeerNames) {
+        return reindeerNames.sort((a, b) => 
+          a.split(' ')[1] > b.split(' ')[1] ? 1 :
+          a.split(' ')[1] < b.split(' ')[1] ? -1 : 0
+        )
+      }
