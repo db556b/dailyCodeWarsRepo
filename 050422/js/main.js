842f074423d3935a36db290db812d2eedd77c364
db556b <102768499+db556b@users.noreply.github.com>
2022-05-04 13:02:32 -0400

    daily CW
    added Kata:
    Scoring Tests

--- 050422/js/main.js
+++ 050422/js/main.js
@@ -73,3 +73,32 @@ const xMarksTheSpot = (input) => {
           a.split(' ')[1] < b.split(' ')[1] ? -1 : 0
         )
       }
+
+
+ //     Scoring Tests
+// The tests in this kata are to be graded in different ways. A certain number of points will be given for each correct answer and a certain number of points will be deducted for each incorrect answer. For ommitted answers, points will either be awarded, deducted, or no points will be given at all.
+
+// Return the number of points someone has scored on varying tests of different lengths.
+
+// The given parameters will be:
+
+//     An array containing a series of 0s, 1s, and 2s, where 0 is a correct answer, 1 is an omitted answer, and 2 is an incorrect answer.
+//     The points awarded for correct answers
+//     The points awarded for ommitted answers (note that this may be negative)
+//     The points deducted for incorrect answers (hint: this value has to be subtracted)
+
+// Note: The input will always be valid (an array and three numbers)
+      //returns the test score
+function scoreTest(str, right, omit, wrong){
+    let score = 0
+    str.forEach ( e => {
+      if (e === 0){
+        score += right
+      } else if (e === 1){
+        score += omit
+      } else {
+        score -= wrong
+      }
+    })
+    return score
+  }
