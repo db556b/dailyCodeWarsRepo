0d20b81178be4f50e987c8facbd50906db03f9d3
db556b <102768499+db556b@users.noreply.github.com>
2022-05-04 23:55:49 -0400

    daily CW
    added kata:
    Odd-heavy Array

--- 050422/js/main.js
+++ 050422/js/main.js
@@ -141,3 +141,29 @@ function scoreTest(str, right, omit, wrong){
       return num >= 0 ? `+${num}` : num.toString()
     })  
     }
+
+
+
+    // Odd-heavy Array
+    // An array is defined to be odd-heavy if it contains at least one odd element and every element whose value is odd is greater than every even-valued element.
+
+    // eg.
+    
+    // Array [11,4,9,2,8] is odd-heavy 
+    // because:- its odd elements [11,9] are greater than all the even elements [4,2,8]
+    
+    // Array [11,4,9,2,3,10] is not odd-heavy
+    // because:- one of it's even element 10 from [4,2,10] is greater than two of its odd elements [9,3] from [ 11,9,3]
+    
+    // write a function called isOddHeavy or is_odd_heavy that accepts an integer array and returns true if the array is odd-heavy else return false.
+    
+
+    function isOddHeavy(n){
+      let odds = []
+      let evens = []
+      n.forEach(num => num % 2 === 0 ? evens.push(num) : odds.push(num));
+      if(odds.length > 0){
+        return Math.min(...odds) >= Math.max(...evens)
+      }return false
+    }
+    
