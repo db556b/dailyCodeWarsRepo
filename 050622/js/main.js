6f5be7166e70f764ca1ce11a4ae60d3e2eb8bbd4
db556b <102768499+db556b@users.noreply.github.com>
2022-05-06 12:17:12 -0400

    CW update
    added kata:
    ASCII Total

--- 050622/js/main.js
+++ 050622/js/main.js
@@ -5,3 +5,14 @@
 function getChar(c) {
     return String.fromCharCode(c)
   }
+
+//   ASCII Total
+//   You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+//   examples:
+  
+//   uniTotal("a") == 97 uniTotal("aaa") == 291
+  
+  function uniTotal (string) {
+    return string.split('').reduce((a,b) => a + b.charCodeAt(0), 0)
+    }
