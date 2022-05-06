5e57239ea4bed522e17ba4d7986e98d41a59fc06
db556b <102768499+db556b@users.noreply.github.com>
2022-05-06 12:09:36 -0400

    Daily CW update
    added Kata:
    get character from ASCII Value

---
+++ 050622/js/main.js
@@ -0,0 +1,7 @@
+
+// get character from ASCII Value
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+function getChar(c) {
+    return String.fromCharCode(c)
+  }
