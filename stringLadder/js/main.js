7a546cd2771f2a75ced64b0453220042a9a69e32
db556b <102768499+db556b@users.noreply.github.com>
2022-05-09 10:12:54 -0400

    daily CW
    added kata:
    String Merge!

--- stringLadder/js/main.js
+++ stringLadder/js/main.js
@@ -13,3 +13,17 @@
 function mouthSize(animal) {
     return animal.toLowerCase() == 'alligator' ? 'small' : 'wide'
   }
+
+
+
+//   7 kyu string kata
+//   String Merge!
+//   Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+
+  function stringMerge(string1, string2, letter){
+    let arr = []
+    arr.push(string1.slice(0,string1.indexOf(letter)))
+    arr.push(string2.slice(string2.indexOf(letter)))
+    return arr.join('')
+  }
