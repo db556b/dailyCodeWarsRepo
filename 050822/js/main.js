948e966e4202ec769b2ff9ee2f028dd46f6a7ee4
db556b <102768499+db556b@users.noreply.github.com>
2022-05-09 09:20:41 -0400

    daily CW
    added Kata:
    Quarter of the year

---
+++ 050822/js/main.js
@@ -0,0 +1,11 @@
+// Quarter of the year
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+
+const quarterOf = (month) => {
+    const months = {1:1,2:1,3:1,4:2,5:2,6:2,7:3,8:3,9:3,10:4,11:4,12:4}
+    return months[month]
+  }
