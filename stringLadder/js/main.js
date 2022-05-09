3d0f7a072e4d66415103b050f7116f09a9b37dfb
db556b <102768499+db556b@users.noreply.github.com>
2022-05-09 10:15:11 -0400

    CW update
    refactored kata:
    string merge!

--- stringLadder/js/main.js
+++ stringLadder/js/main.js
@@ -21,9 +21,8 @@ function mouthSize(animal) {
 //   Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
 
 
-  function stringMerge(string1, string2, letter){
-    let arr = []
-    arr.push(string1.slice(0,string1.indexOf(letter)))
-    arr.push(string2.slice(string2.indexOf(letter)))
-    return arr.join('')
+function stringMerge(string1, string2, letter){
+    return string1.slice(0,string1.indexOf(letter)) + string2.slice(string2.indexOf(letter))
   }
+
+  
