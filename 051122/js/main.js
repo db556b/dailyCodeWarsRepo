9ca601f63c6f1ec39d425c6aeacf616048088ab6
db556b <102768499+db556b@users.noreply.github.com>
2022-05-11 13:45:20 -0400

    daily CW
    added Kata:
    Calculate BMI

--- 051122/js/main.js
+++ 051122/js/main.js
@@ -76,3 +76,24 @@ function twiceAsOld(dad, son) {
   function paperwork(n, m) {
     return n > 0 && m > 0 ? n * m : 0
   }
+
+//   Calculate BMI
+//   Write function bmi that calculates body mass index (bmi = weight / height2).
+
+//   if bmi <= 18.5 return "Underweight"
+  
+//   if bmi <= 25.0 return "Normal"
+  
+//   if bmi <= 30.0 return "Overweight"
+  
+//   if bmi > 30 return "Obese"
+  
+
+
+  function bmi(weight, height) {
+    let bmi = weight/height**2
+    return bmi <= 18.5 ? "Underweight" :
+           bmi <= 25 ? "Normal" :
+           bmi <= 30 ? "Overweight" :
+                        "Obese"
+  }
