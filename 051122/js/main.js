f0d297c5acc5ceb30e6831bbf9f56b8d74397836
db556b <102768499+db556b@users.noreply.github.com>
2022-05-11 13:37:34 -0400

    daily Cw
    added kata:
    Twice as old

--- 051122/js/main.js
+++ 051122/js/main.js
@@ -53,3 +53,16 @@ function duplicateEncode2(word){
     return Math.max(...arr)
     
   }
+
+//   Twice as old
+//   Your function takes two arguments:
+
+//     current father's age (years)
+//     current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+
+function twiceAsOld(dad, son) {
+    return Math.abs(dad - son * 2);
+  }
