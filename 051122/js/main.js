521734fd151b859cc16d907f15c418a7d68daea4
db556b <102768499+db556b@users.noreply.github.com>
2022-05-11 14:00:07 -0400

    daily CW
    Added kata:
    ATM

--- 051122/js/main.js
+++ 051122/js/main.js
@@ -97,3 +97,27 @@ function twiceAsOld(dad, son) {
            bmi <= 30 ? "Overweight" :
                         "Obese"
   }
+
+
+
+// ATM
+//   An ATM has banknotes of nominal values 10, 20, 50, 100, 200 and 500 dollars. You can consider that there is a large enough supply of each of these banknotes.
+
+// You have to write the ATM's function that determines the minimal number of banknotes needed to honor a withdrawal of n dollars, with 1 <= n <= 1500.
+
+// Return that number, or -1 if it is impossible.
+
+// Good Luck!!!
+
+  function solve(n) {
+    let bills = [500,200,100,50,20,10]
+    let acc = 0
+    if (n % 10 != 0){
+      return -1
+    }
+    bills.forEach(e => {
+      acc += Math.floor(n/e)
+      n %= e
+    })
+    return acc
+  }
