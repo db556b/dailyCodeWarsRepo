a0a362e620da0b96664e733be35c8d91875d4402
db556b <102768499+db556b@users.noreply.github.com>
2022-05-11 14:08:16 -0400

    Daily CW update
    added kata:
    Sum of numerous arguments

--- 051122/js/main.js
+++ 051122/js/main.js
@@ -121,3 +121,9 @@ function twiceAsOld(dad, son) {
     })
     return acc
   }
+
+//   Sum of numerous arguments
+//   After calling the function findSum() with any number of non-negative integer arguments, it should return the sum of all those arguments. If no arguments are given, the function should return 0, if negative arguments are given, it should return -1.
+  function findSum(...args){
+    return  Math.min(...args) < 0 ? -1 : args.reduce((a,b) => a + b, 0)
+   }
