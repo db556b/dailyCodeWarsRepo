123b685e36036e91cf6ee7265a5c68cd55a03f0c
db556b <102768499+db556b@users.noreply.github.com>
2022-05-12 14:08:33 -0400

    daily CW update
    added Kata:
    Calculate Parity bit!

--- 051222/js/main.js
+++ 051222/js/main.js
@@ -88,7 +88,20 @@ function even_or_odd(number) {
         result = result
       } else if ( i % 2 === 1 && e === 'o' || e === ' '){
         result = result
-      } else { result = false}
+      } else { result = false }
     })
     return result
+  }
+
+
+
+//   Calculate Parity bit!
+//   A parity bit, or check bit, is a bit added to a string of bits to ensure that the total number of 1-bits in the string is even or odd. Parity bits are used as the simplest form of error detecting code.
+
+//   You have two parameters, one being the wanted parity (always 'even' or 'odd'), and the other being the binary representation of the number you want to check.
+  
+//   Your task is to return an integer (0 or 1), whose parity bit you need to add to the binary representation so that the parity of the resulting string is as expected.
+  function checkParity(parity, bin){
+    let sum = bin.split('').reduce((a,b)=>a+ +b, 0) % 2
+    return parity === 'even' && sum === 1 || parity === 'odd' && sum === 0 ? 1 : 0 
   }
