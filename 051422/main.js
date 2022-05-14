17863f0496275535ec9d80d7d88c12ab229a3ad8
db556b <102768499+db556b@users.noreply.github.com>
2022-05-14 14:17:26 -0400

    CW daily update

---
+++ 051422/main.js
@@ -0,0 +1,21 @@
+// parameters- two positive integers
+// return- return the largest number by which both parameters are divisible
+// examples- below
+// PseudoCode- below
+
+
+
+function mygcd(a,b){
+    //Euclid algorithm needs to be used
+    //check to see if either number is 0
+    if(a === 0) { return b;}
+    if(b === 0) { return a;}
+    // if neither number is 0 use recursion to call the same function passing b as the argument and then taking the remainder of dividing a by b as the second argument
+    //this works as a loop passing the arguments into the same function over and over until one of the two if statements become true. 
+    return mygcd(b, a % b)
+}
+
+
+mygcd(6,9) // should return 3
+mygcd(12,4) // should return 2
+mygcd(24,12) // should return 6
