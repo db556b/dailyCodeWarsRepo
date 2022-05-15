f0298057fa762cd951ea4a9c581de3f2301499ec
db556b <102768499+db556b@users.noreply.github.com>
2022-05-15 10:47:13 -0400

    daily CW
    added kata:
    Get Nth Even Number

---
+++ 051522/main.js
@@ -0,0 +1,16 @@
+// parameters- single whole number  
+// return returns the nth even number starting count at 0   
+// examples-below
+// PseudoCode -below
+
+
+function nthEven(n){
+    //will always return the number times 2 - 2
+    //loop through numbers adding 2 to i every loop
+    return n * 2 - 2
+  }
+
+  nthEven(1) // returns 0
+  nthEven(3) // returns 4
+  nthEven(100) //returns 198
+  nthEven(12987340) // returns 2597466)
