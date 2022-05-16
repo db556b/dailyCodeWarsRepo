a1774adceed6a095a39fcd415b07fa6d39d902f0
db556b <102768499+db556b@users.noreply.github.com>
2022-05-16 10:48:36 -0400

    daily CW update
    added description for KAta:
    get nth numnber

--- 051522/main.js
+++ 051522/main.js
@@ -3,9 +3,22 @@
 // examples-below
 // PseudoCode -below
 
+//Get Nth Even Number
+// Return the Nth Even Number
+
+// Example(Input --> Output)
+
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+
+// The input will not be 0.
+
+
 
 function nthEven(n){
-    //will always return the number times 2 - 2
+    //will always return the number times 2 - 2 except in the instance of negative numbers and 0
     //loop through numbers adding 2 to i every loop
     return n * 2 - 2
   }
