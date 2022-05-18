ed3989855971da6e39a7014851ede392b4b2857b
db556b <102768499+db556b@users.noreply.github.com>
2022-05-18 09:02:17 -0400

    daily CW
    added Kata:
    Grasshopper - Messi Goal

---
+++ 051822/main.js
@@ -0,0 +1,17 @@
+// Grasshopper - Messi Goal
+// Task
+
+//     Create these three variables and store the appropriate values using the table above:
+
+//     laLigaGoals
+//     championsLeagueGoals
+//     copaDelReyGoals
+
+//     Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
