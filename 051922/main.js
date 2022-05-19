4123381717133564b4cc84ac1d1f1369f8e7164a
db556b <102768499+db556b@users.noreply.github.com>
2022-05-19 09:11:18 -0400

    daily CW update
    added comments to kata

--- 051922/main.js
+++ 051922/main.js
@@ -27,14 +27,27 @@
 
 
 function squareIt(int) {
+    //convert int to string
     int = int.toString().split('')
+    //going to be using the sqrt of the length of variable 'int' multiple times, so save that number into a variable - called sqrt
     const sqrt = Math.sqrt(int.length)
-    console.log(sqrt)
+    //test if sqrt is an integer or that sqrt rounded down to nearest integer equals the sqrt
 	if (sqrt === Math.floor(sqrt)){
+        //once verified as an integer, use a for loop to loop through the array to insert the '\n' for line breaks once the array is joined back into a string
     for (let i = sqrt; i < int.length-1; i+=sqrt+1){
+        //insert the '\n' for line breaks once the array is joined back into a string
       int.splice(i,0,`\n`)
     }
+    //return the array joined back into a string
     return int.join('')
   }
+  //if above conditional statement is falsy, then the sqrt(int.length) is not an integer; return 'Not a perfect square!""
   return "Not a perfect square!"
 }
+
+
+squareIt(1)//should return "1" because int.length === 1 and sqrt(1)=== integer of 1
+squareIt(222)// shoudl return "Not a perfect square!" because int.length === 3 and sqrt(3) !== an integer
+squareIt(234562342342)// should return "Not a perfect square!" because int.length === 13 and sqrt(13) !== an integer
+squareIt(88989)// should return "Not a perfect square!" because int.length === 5 and sqrt(5) !== an integer
+squareIt(112141568)// should return "112\n141\n568" because int.length === 9 and sqrt(9)=== integer of 3
