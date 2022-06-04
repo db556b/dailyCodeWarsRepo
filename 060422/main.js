824818bbef5f7acd1aceaf3002ad885da463787c
db556b <davidbuchan900@gmail.com>
2022-06-04 17:06:11 -0400

    added instructions for kata and name of kata

--- 060422/main.js
+++ 060422/main.js
@@ -3,6 +3,8 @@
 // examples
 // PseudoCode
 
+// Enumerable Magic #2 - True for Any?
+// Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
 
 
 function any(arr, fun){
