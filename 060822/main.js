b73c0fdd25fa0644c9b97dd2e572ba34362be1a9
db556b <davidbuchan900@gmail.com>
2022-06-08 14:07:49 -0400

    added kata Pair of gloves to daily CW

---
+++ 060822/main.js
@@ -0,0 +1,34 @@
+// parameters- one array with strings inside
+// returnas- the number of matching pairs of items in the array
+// examples
+// PseudoCode 
+
+
+
+function numberOfPairs(gloves){
+    //copy and sort gloves array into a new variable to not destroy or alter the orginal array
+    let gloves2 = gloves.slice(0).sort()
+    //set acc to 0 for later return 
+    let acc = 0
+    //initiate loop through sorted array
+    for (let i = 0; i < gloves2.length - 1; i++){
+        //check current element against the next element for a match
+      if (gloves2[i] === gloves2[i + 1]){
+          //increase accumulator
+        acc++
+        //remove current element and the next element in the array
+        gloves2.splice(i,2)
+        //decrement i when a match is found to not bypass anything after the elements have been removed
+        i--
+      }
+    }
+    //return acc
+    return acc
+  }
+
+
+  numberOfPairs(['red','red'])  //should return 1 because there is only one pair (red,red)
+  numberOfPairs(['red','green','blue']) //should return 0 because there are no pairs in the array
+  numberOfPairs(['gray','black','purple','purple','gray','black']) //should return 3 because there are three pairs (gray,grey/purple,purpler/black,black)
+
+
