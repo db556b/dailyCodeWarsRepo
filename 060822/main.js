5f8eea7b3fe8cfe1daeeefc33da731255d3a0120
db556b <davidbuchan900@gmail.com>
2022-06-08 14:08:34 -0400

    added description

--- 060822/main.js
+++ 060822/main.js
@@ -3,7 +3,12 @@
 // examples
 // PseudoCode 
 
+// Pair of gloves
+// Pair of gloves
 
+// Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+// Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
 
 function numberOfPairs(gloves){
     //copy and sort gloves array into a new variable to not destroy or alter the orginal array
