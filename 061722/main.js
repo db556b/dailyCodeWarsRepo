ff5652a4cb3490701f9fbc35e77c4dc56b8d4d20
db556b <davidbuchan900@gmail.com>
2022-06-17 15:41:01 -0400

    daily CW update- added kataConsecutive Vowels in a String

--- 061722/main.js
+++ 061722/main.js
@@ -28,3 +28,51 @@ function splitTheBill(x) {
     for (let key in a){x[key] = Math.round((x[key] - average) * 100) / 100}
     return a
   }
+
+// P- a string
+// R- a number consisting of the consecutive vowels (in order) in the string
+// E
+// P
+
+
+// Consecutive Vowels in a String
+//   Return an integer with the length of the consecutive vowels found.
+
+//   This is better explained with a couple of examples:
+  
+//       You are given the string "agrtertyfikfmroyrntbvsukldkfa". The logic is that you start from the "a" and make your way right. The next vowel is an "e" and it is the consecutive vowel, then "i" and so forth until you get to "u". If you keep moving right you find "a" which happens to be the consecutive vowel. Return 6.
+//       This is a slightly trickier example: you are given the string "erfaiekjudhyfimngukduo". As you move right you ignore all vowels until you get to the "a", then ignore the rest until you get to the "e", which is the consecutive vowel and so forth until you get to the "o". Return 4.
+  
+
+  function getTheVowels(word) {
+    //set accumulator, total (for return), and vowels to compare
+    let acc = 0,
+        tot = 0
+    const comp = ['a', 'e', 'i', 'o', 'u']
+    //split word and loop through each element
+    word.split('').forEach(e => {
+      //verify that acc is not larger than the length of comp var
+      if (acc > 4){acc = 0}
+      //compare element to comp accumulator
+      if (e === comp[acc]) {
+        //increment acc and tot when match found
+        acc++
+        tot++
+      }
+    })
+    //return total
+    return tot;
+  }
+
+
+
+
+  
+let s = 'desrehakkjfuteknvfiyrtfbehjdjrobchrunbcbbhdhehbvudjsnanbakkjndhfjenfndinmfnbfondndendnfudnfnanndhdemdmcnfdemnfjimdfofnmfnfjanmdnhdu
+getTheVowels(s) // should return => 16 because there are 16 vowels in a row in the passed string. 
+
+s = 'sudnfhrakekdhhfkakjdjdhvneidkvnudntomcnnamjemdmfudkfhjamvcjedkfdnridnmnbvfhbdjdidncbvchencchdjdodncvchfndnrnencncnffduncbhjdfja
+getTheVowels(s) // should return => 11 because there are 11 vowels in a row in the passed string. 
+
+s = 'jurjfdleiifjriisiouajjfyhekkfjvnnmsuimsnvyuhvcodnmfnsumvbjshhsadkvhfeixou'
+getTheVowels(s) // should return => 11 because there are 11 vowels in a row in the passed string. 
