793c5ca27f37dbccd11918666f6e23f498ab4ad2
db556b <davidbuchan900@gmail.com>
2022-06-23 11:56:26 -0400

    added kata: FizzBuzz++

--- 062322/main.js
+++ 062322/main.js
@@ -45,3 +45,40 @@ function sortArray(array) {
 sortArray([5, 3, 2, 8, 1, 4]) //should return =>  [1, 3, 2, 8, 5, 4]);
 sortArray([5, 3, 1, 8, 0]) //should return =>  [1, 3, 5, 8, 0]);
 sortArray([]) //should return => []);
+
+
+
+
+
+
+// FizzBuzz++
+// There is a common problem given to interviewees in software. It is called FizzBuzz. It works as follows: For the numbers between 1 and 100, print fizz if it is a multiple of 3 and buzz if it is a mutiple of 5, else print the number itself.
+
+// You are in an interview and they ask you to complete fizzbuzz (which can be done in a one-liner in a few langs) and you knock it out of the park.
+
+// Surprised by your ability, the interviewer gives you a harder problem. Given a list of coprime numbers, (that is that the g.c.d. of all the numbers == 1) and an equally sized list of words. compute its fizzbuzz representation up until the pattern of strings repeats itself. 
+
+
+
+function fizzbuzzPlusPlus(numbers, words) {
+    //init arr for pushing and returning
+    let arr = [],
+    //init length by reducing numbers in numbers param
+        length = numbers.reduce((a,b) => a * b)
+    //init loop for pushing words || nums into arr
+    for (let i = 1; i <= length; i++){
+        //init new string to build word(s) for pushing into arr when relevant
+      let str = ''
+      //init new loop to check for rmod of number in numbers and build string
+      for (let j = 0; j < words.length; j++){
+        if (i % numbers[j] === 0){str += words[j]}
+      }
+      //push str into arr. If str is empty, push number
+      arr.push(str || i)
+    }
+    //return arr
+    return arr
+  }
+
+
+  fizzbuzzPlusPlus([2,3,5],["fizz", "buzz", "bazz"]) // should return [ 1, 'fizz', 'buzz', 'fizz', 'bazz', 'fizzbuzz', 7, 'fizz', 'buzz', 'fizzbazz', 11, 'fizzbuzz',  13, 'fizz', 'buzzbazz', 'fizz', 17, 'fizzbuzz',  19, 'fizzbazz', 'buzz', 'fizz', 23, 'fizzbuzz',  'bazz', 'fizz', 'buzz', 'fizz', 29 , 'fizzbuzzbazz'])
