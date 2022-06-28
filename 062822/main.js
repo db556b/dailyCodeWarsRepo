b0849fcac05309c975d1d4f605ad5ee99dbe8fa2
db556b <davidbuchan900@gmail.com>
2022-06-28 13:58:38 -0400

    added kata: Crash Override

--- 062822/main.js
+++ 062822/main.js
@@ -80,3 +80,26 @@ function highAndLow(numbers){
     }
       return asc === true && des === true ? 'no' : asc === true ? 'yes, ascending' : 'yes, descending'
     }
+
+
+
+//     Crash Override
+//     Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+//     Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+//     If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+//     Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+
+
+
+    function aliasGen(f,l){
+        let first = f.toUpperCase()[0],
+            last = l.toUpperCase()[0]
+        return !firstName[first] || !surname[last]  ? "Your name must start with a letter from A - Z." : `${firstName[first]} ${surname[last]}` 
+    
+    }
