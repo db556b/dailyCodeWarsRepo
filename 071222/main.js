fe210369e539c5b484ca383d71bd9ec01b989749
db556b <davidbuchan900@gmail.com>
2022-07-12 14:08:29 -0400

    daily codewars- added leetcode Length of Last Word

--- 071222/main.js
+++ 071222/main.js
@@ -57,6 +57,69 @@ var isValid = function(s) {
 }
 
 
-isValid("()")//should return true as all chars in the array are properly closedd
-isValid("()[]{}")//should return true as all chars in the array are properly closedd
-isValid("(]")//should return false as the '(' is not properly closed with a ')' and is instead closed with a ']'
+//  isValid("()")      //should return true as all chars in the array are properly closedd
+//  isValid("()[]{}")      //should return true as all chars in the array are properly closedd
+//  isValid("(]")      //should return false as the '(' is not properly closed with a ')' and is instead closed with a ']'
+
+
+
+var removeDuplicates = function(nums) {
+    let i = 0,
+        length = nums.length
+    for (let i = 0; i<length; i++){
+        if (i !== nums.lastIndexOf(nums[i])){
+           // console.log(`if ${i + 1} ${nums.lastIndexOf(nums[i] - i)}`)
+            nums.splice(i+1, nums.lastIndexOf(nums[i])-i)
+            length = nums.length
+           // console.log(length, nums)
+        }
+        else {console.log(`else ${i}`)}
+    }
+        console.log(nums)
+        return nums
+    };
+
+    console.log(removeDuplicates([1,1,1,1,1,2,3,3,3,3,4,5,5,5,5,7,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9]))
+
+
+    // Length of Last Word
+    // Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+    // A word is a maximal substring consisting of non-space characters only.
+    
+     
+    
+    // Example 1:
+    
+    // Input: s = "Hello World"
+    // Output: 5
+    // Explanation: The last word is "World" with length 5.
+    
+    // Example 2:
+    
+    // Input: s = "   fly me   to   the moon  "
+    // Output: 4
+    // Explanation: The last word is "moon" with length 4.
+    
+    // Example 3:
+    
+    // Input: s = "luffy is still joyboy"
+    // Output: 6
+    // Explanation: The last word is "joyboy" with length 6.
+    
+     
+    
+    // Constraints:
+    
+    //     1 <= s.length <= 104
+    //     s consists of only English letters and spaces ' '.
+    //     There will be at least one word in s.
+    
+    
+
+    var lengthOfLastWord = function(s) {
+        //take string and trim it to remove trailing spaces. then split it
+        s = s.trim().split(' ')
+        //return the length of the last element in the split array
+        return s[s.length - 1].length
+    };
