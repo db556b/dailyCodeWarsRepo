575f6e14c2b98517d708ca8c864b34e4f6806cb0
db556b <davidbuchan900@gmail.com>
2022-07-12 15:29:23 -0400

    daily codewars added leetcode Implement strStr()

--- 071222/main.js
+++ 071222/main.js
@@ -163,3 +163,71 @@ var removeDuplicates = function(nums) {
         //ummmm...... return the indexOf the needle from the string haystack
         return haystack.indexOf(needle)
     };
+
+
+
+    // Plus One
+    // You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+    // Increment the large integer by one and return the resulting array of digits.
+    
+     
+    
+    // Example 1:
+    
+    // Input: digits = [1,2,3]
+    // Output: [1,2,4]
+    // Explanation: The array represents the integer 123.
+    // Incrementing by one gives 123 + 1 = 124.
+    // Thus, the result should be [1,2,4].
+    
+    // Example 2:
+    
+    // Input: digits = [4,3,2,1]
+    // Output: [4,3,2,2]
+    // Explanation: The array represents the integer 4321.
+    // Incrementing by one gives 4321 + 1 = 4322.
+    // Thus, the result should be [4,3,2,2].
+    
+    // Example 3:
+    
+    // Input: digits = [9]
+    // Output: [1,0]
+    // Explanation: The array represents the integer 9.
+    // Incrementing by one gives 9 + 1 = 10.
+    // Thus, the result should be [1,0].
+    
+     
+    
+    // Constraints:
+    
+    //     1 <= digits.length <= 100
+    //     0 <= digits[i] <= 9
+    //     digits does not contain any leading 0's.
+    
+    
+
+    var plusOne = function(digits) {
+        //loop through argument starting at the end
+        for (let i = digits.length - 1; i >= 0; i--){
+            //if element is the first element and that element is 9
+            if (i === 0 && digits[i] === 9){
+                //change current element to 0
+                digits[i] = 0
+                //unshift a 1 to the beginning of the array
+                digits.unshift(1)
+                //return argument
+                return digits
+                //if element is 9
+            } else if (digits[i] === 9){
+                //change to 0 and restart loop from beginning
+                digits[i] = 0
+                //if no if staments are true
+            } else {
+                //add one to the current element
+                digits[i]++;
+                //return argument with change
+                return digits
+            }
+        }
+        };
