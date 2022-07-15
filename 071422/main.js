e02815ea74e618664dc41db86cf32a97c191c283
db556b <davidbuchan900@gmail.com>
2022-07-14 21:27:22 -0400

    daily codewars update. added kata: Mumbling

--- 071422/main.js
+++ 071422/main.js
@@ -16,3 +16,49 @@ function findShort(s){
 findShort("bitcoin take over the world maybe who knows perhaps") // should return 3 becaue that is the length of the shortest word in the argument
 findShort("turns out random test cases are easier than writing out basic ones") // should return 3 becaue that is the length of the shortest word in the argument 
 findShort("Let's travel abroad shall we") // should return 2 becaue that is the length of the shortest word in the argument
+
+
+
+
+// Create Phone Number
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// Example
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// The returned format must be correct in order to complete this challenge.
+// Don't forget the space after the closing parentheses!
+
+
+
+function createPhoneNumber(n){
+    return `(${n.slice(0,3).join('')}) ${n.slice(3,6).join('')}-${n.slice(6).join('')}`
+  }
+
+//   Mumbling
+//   This time no story, no theory. The examples below show you how to write function accum:
+//   Examples:
+  
+//   accum("abcd") -> "A-Bb-Ccc-Dddd"
+//   accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//   accum("cwAt") -> "C-Ww-Aaa-Tttt"
+  
+//   The parameter of accum is a string which includes only letters from a..z and A..Z.
+  
+
+  function accum(s) {
+    let string = []
+    s = s.split('')
+    s.forEach((e,i) => {
+      let str = `${e.toUpperCase()}` 
+      for (let j = 0; j < i; j++){
+        str += e.toLowerCase()
+      }
+      string.push(str)
+    })
+    return string.join('-')
+  }
+
+  function accum2(s) {
+    return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+  }
