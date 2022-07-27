1c04eb135d139534461b2abfdda5afa41ab658e0
db556b <davidbuchan900@gmail.com>
2022-07-27 12:26:27 -0400

    daily CW update. Added kata: Return String of First Characters

--- 072722/main.js
+++ 072722/main.js
@@ -22,3 +22,28 @@ evenChars (";;;--") // should return =>  [";", "-"] as these are the odd indexed
 evenChars ("abcdefghijklm") // should return =>  ["b", "d", "f", "h", "j", "l"] as these are the odd indexed characters in the argument
 
 
+//P- a string of space-separated words
+//R- the first letter of each word as a string
+
+
+// Return String of First Characters
+// In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+// For example:
+// "This Is A Test" ==> "TIAT"
+// Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+
+
+function makeString(s){
+    //take argument and split it to make an array
+    return s.split(' ')
+    //map each of the elements of the array returning only the first element of each of the array's elements (or the first letter of the words)
+            .map(e => e[0])
+            //join them back together
+            .join('')
+  }
+
+
+makeString("sees eyes xray yoat") //should return => "sexy" as these are the first letter of the argument's words
+makeString("brown eyes are nice") //should return => "bean" as these are the first letter of the argument's words
+makeString("cars are very nice") //should return => "cavn" as these are the first letter of the argument's words
+makeString("kaks de gan has a big head") //should return => "kdghabh" as these are the first letter of the argument's words
