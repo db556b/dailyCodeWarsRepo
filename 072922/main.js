f21de615e82a1b6ca595fb90f7fb8dd37a3cac2a
db556b <davidbuchan900@gmail.com>
2022-07-29 20:22:24 -0400

    daily CW update: added kata- Largest pair sum in array

--- 072922/main.js
+++ 072922/main.js
@@ -79,3 +79,34 @@ alphabetWar("zdqmwpbs"); //should return => 'Let's fight again!'
 alphabetWar("zzzzs");    //should return => 'Right side wins!'
 alphabetWar("wwwwwwz");  //should return => 'Left side wins!'
 
+
+//P- an array of numbers
+//R- the sum of the largest two numbers in the argument array
+
+// Largest pair sum in array
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+// Input sequence contains minimum two elements and every element is an integer.
+
+
+function largestPairSum (numbers) {
+    //return numbers
+    return numbers
+    //sorted from lowest to highest
+            .sort((a,b) => a - b)
+    //splice the final two numbers in the array
+            .splice(numbers.length - 2)
+    //reduce the spliced numbers to make a total of the highest two numbers in the array
+            .reduce((a,b) => a + b,0)
+  }
+
+  
+largestPairSum(10,14,2,23,19) // should return =>  42
+largestPairSum(-100,-29,-24,-19,19) // should return =>  0
+largestPairSum(1,2,3,4,6,-1,2) // should return =>  10
+largestPairSum(-10, -8, -16, -18, -19) // should return =>  -18
