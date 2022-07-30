2a659f80f7fc65a5cc236e39c30d6dcfb6ba5843
db556b <davidbuchan900@gmail.com>
2022-07-29 20:09:59 -0400

    daily codewars update. added kata: Alphabet war

--- 072922/main.js
+++ 072922/main.js
@@ -24,3 +24,58 @@ lastSurvivor('kbc', [0, 1]) // should return => 'b'
 lastSurvivor('zbk', [2, 1]) // should return => 'z'
 lastSurvivor('c', []) // should return => 'c'
 lastSurvivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr', [8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1]) // should return => 'd'
+
+
+
+// Alphabet war
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+// Task
+
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+
+// The other letters don't have power and are only victims.
+// Example
+
+
+function alphabetWar(fight){
+    let left = {
+      w : 4,
+      p : 3,
+      b : 2,
+      s : 1
+    },
+    right = {
+      m : 4,
+      q : 3,
+      d : 2,
+      z : 1
+      },
+     leftTot = 0,
+     rightTot = 0
+    fight.split('').forEach(e => {
+      left.hasOwnProperty(e) ? leftTot += left[e] : right.hasOwnProperty(e) ? rightTot += right[e] : false
+    })
+     return leftTot > rightTot ? "Left side wins!" : rightTot > leftTot ? "Right side wins!" : "Let's fight again!";
+  }
+
+
+alphabetWar("z");        //should return => 'Right side wins!'
+alphabetWar("zdqmwpbs"); //should return => 'Let's fight again!'
+alphabetWar("zzzzs");    //should return => 'Right side wins!'
+alphabetWar("wwwwwwz");  //should return => 'Left side wins!'
+
