0e368bd05b824dbd0d9240024439512cca9c61cb
db556b <davidbuchan900@gmail.com>
2022-07-31 19:47:49 -0400

    daily cw update. Added kata: The First Non Repeated Character In A String

---
+++ 073122/main.js
@@ -0,0 +1,48 @@
+// parameters- a string of letter or numbers    
+// return- the first letter or number that occurs onlyt one time. If none are non-repeating, return null
+// examples
+// PseudoCode
+
+// The First Non Repeated Character In A String 
+// You need to write a function, that returns the first non-repeated character in the given string.
+
+// If all the characters are unique, return the first character of the string.
+// If there is no unique character, return null in JS or Java, and None in Python.
+
+// You can assume, that the input string has always non-zero length.
+
+
+
+function firstNonRepeated(s) {
+    //init object and result (for return) with a value of null for when there are no repeating elements in the argument
+    let obj = {},
+        result = null
+    //assign key : value pairs into the obj object for each element in the argument 
+    s.split('').forEach(e => obj[e] ? obj[e]++ : obj[e] = 1)
+    //cehck each value in the obj
+  Object.getOwnPropertyNames(obj).some(key =>{
+    //test for value to equal 1
+    if (obj[key] === 1){
+        //assign key to result when value === 1
+      result = key;
+      //return true to stop the loop
+      return true
+    }
+  })
+  //return result
+    return result
+  //   let word = ''
+  //   for (let i = 0; i < s.length; i++){
+  //     if (i === s.lastIndexOf(s[i]) && !word.includes(s[i])) return s[i];
+  //     word += s[i]
+  //   }
+  //   return null
+  }
+
+
+  
+firstNonRepeated("test") // should return 'e' because it is the first element that does not repeat itself in the argument.
+firstNonRepeated("teeter") // should return 'r' because it is the first element that does not repeat itself in the argument.
+firstNonRepeated("1122321235121222") // should return '5' because it is the first element that does not repeat itself in the argument.
+firstNonRepeated("rend") // should return 'r' because it is the first element that does not repeat itself in the argument.
+firstNonRepeated("ttrree") // should return => null because all letters in the argument string repeat
