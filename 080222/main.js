a0590f21d6ee47081e816a55d45456e35ae98f6b
db556b <davidbuchan900@gmail.com>
2022-08-02 21:32:32 -0400

    Valid Parentheses added to daily CW

--- 080222/main.js
+++ 080222/main.js
@@ -4,8 +4,8 @@
 // PseudoCode
 
 
-Valid Parentheses
-Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+// Valid Parentheses
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
 function validParentheses(parens) {
     //init arr for pushing paren in
