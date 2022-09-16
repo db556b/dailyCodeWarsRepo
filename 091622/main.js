c328e52ce6da714c237542080b721cd89e02cf01
db556b <davidbuchan900@gmail.com>
2022-09-16 14:52:46 -0400

    daily cw
    added kata- Unique string characters

---
+++ 091622/main.js
@@ -0,0 +1,22 @@
+
+// Unique string characters
+// In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+
+// For example:
+
+// solve("xyab","xzca") = "ybzc" 
+// --The first string has 'yb' which is not in the second string. 
+// --The second string has 'zc' which is not in the first string. 
+
+// Notice also that you return the characters from the first string concatenated with those from the second string.
+
+
+function loop(a,b,c){
+    a.split('').forEach( e=> !b.includes(e) ? c.push(e) : false )
+  }
+function solve(a,b){
+    let arr = []
+    loop(a,b,arr);
+    loop(b,a,arr);
+    return arr.join('')
+};
