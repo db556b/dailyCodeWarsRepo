db0ed1bea73d941e593e23a02bd2681223007935
db556b <davidbuchan900@gmail.com>
2022-09-28 19:37:41 -0400

    daily cw added kata: reverse words

---
+++ 092822/main.js
@@ -0,0 +1,19 @@
+
+// Reverse words
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Examples
+
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+
+function reverseWords(str) {
+    return str.split(' ').map(e => {
+      return e.split('')
+              .reverse()
+              .join('')
+          })
+        .join(' ');
+    }
